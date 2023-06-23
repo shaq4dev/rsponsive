83e4e69f8dee8914d79884724177b3830f61024e
Shaquille Blackwood <shaquille.blackwood95@gmail.com>
2023-06-23 12:36:08 -0500

    final toggle dark mode complete

--- app.js
+++ app.js
@@ -24,3 +24,36 @@ let date = new Date()
 let currentYear = date.getFullYear()
 
 year.textContent = currentYear
+
+// toggle dark/light
+
+const darkSwitch = document.querySelector('.toDark')
+const lightSwitch = document.querySelector('.toLight')
+
+// elements 
+
+const body = document.body 
+const h3 = document.querySelector('h3')
+const p = document.querySelector('p')
+
+// default styles 
+
+function defaultStyles(){
+    body.style.backgroundColor = 'var(--white)'
+    body.style.color = 'var(--jet-black)'
+    darkSwitch.style.display = 'block'
+    darkSwitch.style.margin = 'auto'
+    lightSwitch.style.display = 'none'
+}
+
+function darkStyles(){
+    body.style.backgroundColor = '#000'
+    body.style.color = '#fff'
+    darkSwitch.style.display = 'none'
+    lightSwitch.style.display = 'block'
+    lightSwitch.style.margin = 'auto'
+
+}
+
+darkSwitch.addEventListener('click', darkStyles)
+lightSwitch.addEventListener('click', defaultStyles)
